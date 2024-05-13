8f757dffd84f7efcc900cd4c6ab28af3eddd0109
Luca-Mariselli <lucamariselli@gmail.com>
2024-05-13 16:43:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+let zucchine = [
+    {varieta:`rossa`, peso:200, Lunghezza: `8`},
+    {varieta:`gialla`, peso:100, Lunghezza: `20`},
+    {varieta:`rossa`, peso:100, Lunghezza: `17`},
+    {varieta:`verde`, peso:22, Lunghezza: `26`},
+    {varieta:`gialla`, peso:231, Lunghezza: `13`},
+    {varieta:`blu`, peso:277, Lunghezza: `11`},
+    {varieta:`viola`, peso:176, Lunghezza: `7`},
+    {varieta:`rossa`, peso:200, Lunghezza: `22`},
+    {varieta:`gialla`, peso:207, Lunghezza: `12`},
+    {varieta:`viola`, peso:222, Lunghezza: `2`}
+];
+
+
+// // SNACK 1
+// let pesoTotale = 0
+
+// for(let i=0; i<zucchine.length; i++){
+//     let pesoZucchina = zucchine[i].peso;
+//     parseInt(pesoZucchina)
+//     pesoTotale = pesoTotale + pesoZucchina;
+// }
+
+// console.log(pesoTotale)
+
+
+// // SNACK 2
+// let zucchinePiccole = []
+// let zucchineGrandi = []
+// let pesoPiccole = 0
+// let pesoGrandi = 0
+// let pesoTotale = 0
+
+
+
+// for(let i=0; i<zucchine.length; i++){
+//     if(zucchine[i].Lunghezza <= 15){
+//         zucchinePiccole.push(zucchine[i])
+//         let pesoPiccola = zucchine[i].peso
+//         parseInt(pesoPiccola)
+//         pesoPiccole = pesoPiccole + pesoPiccola
+        
+//     } else{
+//         zucchineGrandi.push(zucchine[i])
+//         let pesoGrande = zucchine[i].peso
+//         parseInt(pesoGrande)
+//         pesoGrandi = pesoGrandi + pesoGrande
+//     }
+//     pesoTotale = pesoGrandi + pesoPiccole
+// }
+// console.log(`Lista zucchine piccole `, zucchinePiccole)
+// console.log(`Lista zucchine grandi `, zucchineGrandi)
+// console.log(`Peso zucchine piccole `, pesoPiccole)
+// console.log(`Peso zucchine grandi `, pesoGrandi)
+// console.log(`Peso totale `, pesoTotale)
+
+
